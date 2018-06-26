b749359065919281a7525ae79a8db77cfec49b97
Victor <unicaxverdad@gmail.com>
2018-06-26 14:57:12 +0300

    setup the router module
    setup the router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { BlogpostComponent } from './components/blogpost/blogpost.component';
+import { PlanningToolsComponent } from './components/planning-tools/planning-tools.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomePageComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: 'planning-tools', component: PlanningToolsComponent },
+  { path: 'blogpost', component: BlogpostComponent },
 ];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ],
 })
 export class AppRoutingModule {}
