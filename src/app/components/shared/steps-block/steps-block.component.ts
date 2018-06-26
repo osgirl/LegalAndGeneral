c02fbb7057ac87b9f7c228007297837e89a71673
Victor <unicaxverdad@gmail.com>
2018-06-26 13:21:42 +0300

    step images switching logic
    step images switching logic

--- src/app/components/shared/steps-block/steps-block.component.ts
+++ src/app/components/shared/steps-block/steps-block.component.ts
@@ -1,3 +1,5 @@
+declare let require: any;
+
 import { Component, OnInit } from '@angular/core';
 import { StepList } from './step-list';
 import { StepItem } from './step-item';
@@ -9,14 +11,23 @@ import { StepItem } from './step-item';
 })
 export class StepsBlockComponent implements OnInit {
   stepList = StepList;
+  stepPhotoHeaderPath = require("./images/step-photo.jpg");
+  photoPathList = [
+    require("./images/step-photo_1.jpg"),
+    require("./images/step-photo_2.jpg"),
+    require("./images/step-photo_3.jpg"),
+    require("./images/step-photo_4.jpg"),
+  ];
+  activeStep: number;
   selectedItem: StepItem;
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSelect(item: StepItem) {
+  onSelect(item: StepItem, index: number) {
     this.selectedItem = item;
+    this.activeStep = index;
   }
   closeItem(event: any) {
     event.stopPropagation();
