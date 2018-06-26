7a386bfac98c42b1b9aa4eb8e5f59d93501a9dcf
Victor <unicaxverdad@gmail.com>
2018-06-26 13:47:02 +0300

    add menu items
    add menu items

--- src/app/components/shared/header/first-level-items-list.ts
+++ src/app/components/shared/header/first-level-items-list.ts
@@ -1,9 +1,9 @@
 import { FirstLevelItem } from './first-level-item';
 
 export const FirstLevelItems: FirstLevelItem[] = [
-    { text: 'First step' },
+    { text: 'Planning tools' },
     {
-      text: 'On your way',
+      text: 'Options',
 
       secondLevelItems: [
         {
@@ -12,6 +12,7 @@ export const FirstLevelItems: FirstLevelItem[] = [
             { text: 'Managing your pension pot' },
             { text: 'Reviewing your choices' },
             { text: 'Getting you pensions into one place' },
+            { text: 'Save your ' },
           ]
         },
         {
@@ -22,7 +23,8 @@ export const FirstLevelItems: FirstLevelItem[] = [
               thirdLevelItems: [
                 { text: 'Balancing risk and reward' },
                 { text: 'Making your own investment choices' },
-                { text: 'Your investment options' }
+                { text: 'Your investment options' },
+                { text: 'Bringing your pension savings together' }
               ]
             }
           ]
@@ -36,7 +38,8 @@ export const FirstLevelItems: FirstLevelItem[] = [
               thirdLevelItems: [
                 { text: 'How I can manage my pension savings' },
                 { text: 'What I need to think about now' },
-                { text: 'How to get my pensions in one place' }
+                { text: 'How to get my pensions in one place' },
+                { text: 'Which investment choices do I have' }
               ]
             }
           ]
@@ -50,23 +53,29 @@ export const FirstLevelItems: FirstLevelItem[] = [
       secondLevelItems: [
         {
           column: [
-            { text: 'My insurance' },
+            { text: 'My insurance', isNavHeading: true },
             { text: 'Landlord insurance' },
-            { text: 'Travel insurance' }
+            { text: 'Travel insurance' },
+            { text: 'Pet insurance' },
+            { text: 'Medicare insurance' }
           ]
         },
         {
           column: [
             { text: 'Car insurance' },
             { text: 'Lifestyle insurance' },
-            { text: 'Pet insurance' }
+            { text: 'Home insurance' },
+            { text: 'Pet insurance' },
+            { text: 'Tenants insurance' },
           ]
         },
         {
           column: [
-            { text: 'Tenants insurance' },
-            { text: 'Medicare insurance' },
-            { text: 'DEFAQTO' }
+            { text: 'Make your plan', isNavHeading: true },
+            { text: 'Choose from our three' },
+            { text: 'Get a quote for insurance' },
+            { text: 'Your insurance options' },
+            { text: 'Ask a question' },
           ]
         }
       ]
