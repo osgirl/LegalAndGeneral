d80e2212ab2213c486ac2965a311381f88cda51f
Victor <unicaxverdad@gmail.com>
2018-06-27 16:40:08 +0300

    ++list items
    added list of carousel items

---
+++ src/app/components/shared/image-carousel/carousel-item-list.ts
@@ -0,0 +1,27 @@
+export const CarouselItemList = [
+  {
+    titleLink: { text: "Managing your pension pot", url:"#" },
+    text: "You should try and keep an eye on your pension pot as you get on with other things too",
+    image: "carousel-photo-1.jpg"
+  },
+  {
+    titleLink: { text: "Managing your pension pot", url:"#" },
+    text: "You should try and keep an eye on your pension pot as you get on with other things too",
+    image: "carousel-photo-2.jpg"
+  },
+  {
+    titleLink: { text: "Managing your pension pot", url:"#" },
+    text: "You should try and keep an eye on your pension pot as you get on with other things too",
+    image: "carousel-photo-3.jpg"
+  },
+  {
+    titleLink: { text: "Managing your pension pot", url:"#" },
+    text: "You should try and keep an eye on your pension pot as you get on with other things too",
+    image: "carousel-photo-4.jpg"
+  },
+  {
+    titleLink: { text: "Managing your pension pot", url:"#" },
+    text: "You should try and keep an eye on your pension pot as you get on with other things too",
+    image: "carousel-photo-5.jpg"
+  },
+]
